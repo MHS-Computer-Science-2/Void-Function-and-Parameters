0b09de3a20275fcaedaf7ebcd6664cd3b0ca0695
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2023-02-16 07:35:31 -0500

    Update complete.cpp

--- complete.cpp
+++ complete.cpp
@@ -1,62 +1,50 @@
 // Void Functions and Parameters
-
-// I CAN write and call functions
-// I CAN explain and use function parameters
-
+// 
+// I CAN write and call void functions
 
 #include <iostream>
 using namespace std;
 
-//** Functions start here **
+
+// Welcome function
+// Displays a greeting message
 void welcome() {
-	cout << "WELCOME TO THE FUNCTION NOTES" << endl;
+	cout << "WELCOME TO THE VOID FUNCTION NOTES" << endl;
 	cout << "*****************************" << endl << endl;
 }
 
-void doubleIt(int num) {
-	cout << num << " doubled is " << num * 2 << endl << endl;
-}
-
-void spam(string msg, int times) {
-	for (int i = 0; i < times; i++) {
+//Display msg n times 
+void spam(string msg, int n) {
+	for (int i = 0; i < n; i++) {
 		cout << msg << endl;
 	}
 }
 
-//** Functions end here **
-
 
 int main()
 {
-	//FUNCTIONS are short tasks that allow us to break
-	//problems into smaller chunks. 
+	//Not all functions produce an output (return value)
+	//These functions are call VOID
 
-	//Every function has an IDENTIFIER used to CALL the
-	//function. When we CALL the funtion, it performs its 
-	//task
-
+	//EXAMPLE 1
 	//Call the welcome function
-	welcome();
 
-	//PARAMTERS allow us to send information to a function
-	//The function uses this information to perform its task
 
-	//Let's call a function that doubles a number
-	doubleIt(723);
+	//Void functions do a job, but do not return an answer
 
-	//Let's call a function that spams the message "Buy Gecko Insurance!" 
-	//20 times
+	//EXAMPLE 2
+	//Let's write and call a function that spams the  
+	//message "Buy Gecko Insurance!" 20 times	
 	spam("Buy Gecko Insurance!", 20);
 
-	//You try: spam the message "Drink Pepsi" 13 times
-	spam("Drink Pepsi", 13);
 
-	//You try: spam the message "Tibor was here" n times, where
-	//         n is a number entered by the user
-	int n;
+	//EXAMPLE 3
+	//You try: spam the message "Drink Pepsi" x times, where n
+	//is a number entered by the user
+	int x;
 	cout << "Enter a number " << endl;
-	cin >> n;
-	spam("Tibor was here", n);
+	cin >> x;
+	spam("Drink Pepsi", x);
 
 
 
